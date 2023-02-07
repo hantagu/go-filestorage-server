280156c86eaf95afe99903610e86f561f52b6be8
Artem Demyanov <46056004+hantagu@users.noreply.github.com>
2023-02-07 19:20:10 +0300

    Intermediate commit

--- protocol/ClaimUsername.go
+++ protocol/ClaimUsername.go
@@ -1,8 +1,6 @@
 package protocol
 
-const (
-	CLAIM_USERNAME = "claim_username"
-)
+const CLAIM_USERNAME = "claim_username"
 
 type ClaimUsername struct {
 	Username string `bson:"username"` // Username to be taken
